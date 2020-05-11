a15b7e1775335a344fe9fd4d6b3b4b9354d594cb
Juan Belieni <juanbelieni@gmail.com>
2020-05-11 17:15:15 -0300

    Finish "carga horaria" report

--- app/Controllers/Http/ReportController.js
+++ app/Controllers/Http/ReportController.js
@@ -5,55 +5,67 @@
 
 const Database = use('Database')
 
-const fieldsNames = {
-  professor: 'Professor',
-  disciplina: 'Disciplina',
-  // carga_horaria: 'Carga horária',
-  // aulas_semana: 'Quantidade de aulas por semana',
-  // duracao_aula: 'Duração da aula',
-  turma: 'Turma',
-  // periodo: 'Período',
-  // ano_semestre: 'Ano/semestre',
-}
-
-const fieldsQueries = {
-  disciplina: 'disciplina as Disciplina',
-  professor: 'professor as Professor',
-  carga_horaria: 'concat(sum(carga_horaria), " minutos") as "Carga horária"',
-  turma: 'turma as Turma',
-  periodo: 'concat(periodo, "º ", if(semestre, "semestre", "ano")) as Período',
-  ano_semestre: 'concat(ano, if(semestre, concat("/", semestre), "")) as "Ano/semestre"',
-  aulas_semana: 'concat(aulas_semana, " aulas") as "Quantidade de aulas por semana"',
-  duracao_aula: 'concat(duracao_aula, " minutos") as "Duração da aula"',
-}
-
 class ReportController {
   /**
    * @param {object} ctx
    * @param {Request} ctx.request
    */
   async cargaHoraria({ request }) {
-    const { fields, turmas, professores } = request.only(['fields', 'turmas', 'professores'])
+    const {
+      option, turmas, professores, ano, semestre,
+    } = request.only(['option', 'turmas', 'professores', 'ano', 'semestre'])
 
-    const select = [
-      ...fields.map((field) => fieldsQueries[field]),
-      Database.raw('concat(convert(sum(carga_horaria), char(50)), " minutos") as "Carga horária"'),
-    ]
-
-    return Database
+    const report = Database
       .table('cargas_horarias')
-      .select(select)
       .whereNotNull('professor')
+
+    switch (option) {
+      case 'default':
+        report
+          .select([
+            'professor as Professor',
+            Database.raw('concat(convert(sum(carga_horaria), char(50)), " minutos") as "Carga horária"'),
+            Database.raw('concat(periodo, "º ", if(semestre, "semestre", "ano")) as Período'),
+            'turma as Turma',
+          ])
+          .groupBy(['professor_id', 'turma_id', 'periodo'])
+          .orderBy(['professor'])
+        break
+      case 'simplified':
+        report
+          .select([
+            'professor as Professor',
+            Database.raw('concat(convert(sum(carga_horaria), char(50)), " minutos") as "Carga horária"'),
+          ])
+          .groupBy(['professor_id'])
+          .orderBy(['professor'])
+        break
+      case 'detailed':
+        report
+          .select([
+            'professor as Professor',
+            Database.raw('concat(carga_horaria, " minutos") as "Carga horária"'),
+            'disciplina as Disciplina',
+            Database.raw('concat(aulas_semana, " aulas") as "Quantidade de aulas por semana"'),
+            Database.raw('concat(duracao_aula, " minutos") as "Duração da aula"'),
+            Database.raw('concat(periodo, "º ", if(semestre, "semestre", "ano")) as Período'),
+            'turma as Turma',
+          ])
+        break
+      default:
+        report.select('*')
+    }
+
+    return report
       .where((query) => {
         if (typeof turmas === 'object') {
           query.whereIn('turma_id', turmas)
         } if (typeof professores === 'object') {
           query.whereIn('professor_id', professores)
         }
-        query.where('ano', 2020)
-        query.whereIn('semestre', [0, 1])
+        query.where('ano', ano)
+        query.whereIn('semestre', [0, semestre])
       })
-      .groupBy(fields.map((field) => `${field}_id`))
   }
 }
 
